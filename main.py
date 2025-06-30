65b154589250874210ac267164a4f0ce545cc1d0
Aras Lamenni <manismi2me@gmail.com>
2025-06-30 08:39:13 +0700

    add login

--- main.py
+++ main.py
@@ -5,14 +5,27 @@ def intro():
    print("++++ FRESH FRUIT BUNCHES GRADING APLICATION ++++")
    print("================================================\n")
 
-def login():
-   username = in
+def read_json(filename):
+   with open(filename, 'r') as file:
+      data = json.load(file)
+      return data
 
-todos = {"todos": ["eat", "sleep", "code"]}
-with open('database_grading.json', 'w') as file:
-   json.dump(todos, file)
+def save_json(filename, db):
+   with open(filename, 'w') as file:
+      json.dump(todos, file)
+      
+def login(db):
+   username = input("username: ")
+   password = input("password: ")
+   if username in db and db[username]['password'] == password:
+      print(f"Welcome back, {username}")
+      return db[username]
+   else:
+      print("username or password not invalid, please register.")
+
    
 
 if __name__ == "__main__":
-  intro()
-   
+   db = read_json("database_grading.json") 
+   intro()
+   login(db)
