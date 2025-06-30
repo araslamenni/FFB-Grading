fc3044dd6bcce1453dbf419de51657a98c5fe4ea
Aras Lamenni <manismi2me@gmail.com>
2025-06-30 08:20:25 +0700

    make intro ffb grading

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
-print('hello world')
-print('hello world')
-print('hello world')
-print('hello world')
+import json
 
+def intro():
+   print("================================================")
+   print("++++ FRESH FRUIT BUNCHES GRADING APLICATION ++++")
+   print("================================================\n")
 
-print("ini adalah fitur dari branch dev ")
+def login():
+   username = in
+
+todos = {"todos": ["eat", "sleep", "code"]}
+with open('database_grading.json', 'w') as file:
+   json.dump(todos, file)
+   
+
+if __name__ == "__main__":
+  intro()
+   
