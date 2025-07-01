373c3f86d52257bba4ab80cacd1c7b8456bd424b
Aras Lamenni <manismi2me@gmail.com>
2025-07-01 08:33:17 +0700

    added register

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import json
 
 def intro():
-   print("================================================")
+   print("\n================================================")
    print("++++ FRESH FRUIT BUNCHES GRADING APLICATION ++++")
    print("================================================\n")
 
@@ -12,7 +12,8 @@ def read_json(filename):
 
 def save_json(filename, db):
    with open(filename, 'w') as file:
-      json.dump(todos, file)
+      json.dump(db, file)
+      
       
 def login(db):
    username = input("username: ")
@@ -23,9 +24,21 @@ def login(db):
    else:
       print("username or password not invalid, please register.")
 
-   
+def register(db):
+   username = input("username: ")
+   password1 = input("password: ")
+   password2 = input("confirm password: ")
+   if username  not in db and password1 == password2:
+      db[username] = {'name': username, 'password': password1, 'grading': []}
+      save_json('database_grading.json', db)
 
 if __name__ == "__main__":
    db = read_json("database_grading.json") 
    intro()
-   login(db)
+   login_or_register = input('login_or_register: ')
+   if login_or_register == 'login':
+      user = login(db)
+   elif login_or_register == 'register':
+      user = register(db)
+   else:
+      print('wrong input')
